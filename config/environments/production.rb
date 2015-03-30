18a39a977c1c4f33978e28e83fc444d575416dde
kadarme <kn.adarme@gmail.com>
2015-03-30 16:35:09 +0000

    Changed environment/production.rb to display all logs.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.log_level = :debug
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
